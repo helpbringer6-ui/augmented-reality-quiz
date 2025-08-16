12a245bc5df4be58b1fec738966b7608a956a8ba
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-08-16 10:06:58 -0500

    Add comments to clarify webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,31 @@
 const path = require('path');
 
 module.exports = {
+    // Entry point for the application
     entry: './src/main.js',
     output: {
+        // Output bundle file
         filename: 'bundle.js',
+        // Output path for the bundle
         path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
+        // Static file serving directory
         contentBase: path.join(__dirname, 'dist'),
+        // Enable gzip compression
         compress: true,
+        // Port for the dev server
         port: 9000,
     },
     module: {
         rules: [
             {
+                // Transpile JavaScript files
                 test: /.js$/, 
+                // Exclude node_modules directory
                 exclude: /node_modules/, 
                 use: {
+                    // Use Babel loader for transpilation
                     loader: 'babel-loader',
                 },
             },
