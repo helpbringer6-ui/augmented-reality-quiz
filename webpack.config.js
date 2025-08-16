f17047df94e647b60875cc430f2a8ebb2f28bec7
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-08-16 10:00:04 -0500

    feat: initial commit for webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /.js$/, 
+                exclude: /node_modules/, 
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+};
