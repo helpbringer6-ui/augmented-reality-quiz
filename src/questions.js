2637324df1087ebcfc1d7f91f4cf1a2fa039151e
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-08-16 09:59:59 -0500

    feat: initial commit for src/questions.js

---
+++ src/questions.js
@@ -0,0 +1,14 @@
+const questions = [
+    {
+        question: 'Was ist die Hauptstadt von Deutschland?',
+        answers: ['Berlin', 'München', 'Hamburg'],
+        correct: 'Berlin'
+    },
+    {
+        question: 'Wie viele Bundesländer hat Deutschland?',
+        answers: ['10', '16', '14'],
+        correct: '16'
+    }
+];
+
+export default questions;
